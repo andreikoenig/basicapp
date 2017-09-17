4bc139fce410b3db8619b7f51f4f2935108187fe
Andrei Koenig <nv1982@gmail.com>
2017-09-17 19:12:21 -0400

    add messages table via migration, contact form save messages to table, dispay success message

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Message;
 
 class MessagesController extends Controller
 {
@@ -11,6 +12,17 @@ class MessagesController extends Controller
         'name' => 'required',
         'email' => 'required'
       ]);
-       return 'success';
+
+       // Create new message
+       $message = new Message; //eloquent
+       $message->name = $request->input('name');
+       $message->email = $request->input('email');
+       $message->message = $request->input('message');
+
+       // Save Message
+       $message->save();
+
+       // Redirect back to home page
+       return redirect('/')->with('success', 'Message Sent');
     }
 }
